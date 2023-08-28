0def5102c32939d5fde239bcaa8934206777ce7a
Nikolai Franz <franztumapon13@gmail.com>
2023-08-28 17:15:36 +0800

    StudentList deleteStudRecordByID

--- Linked List/StudentList.c
+++ Linked List/StudentList.c
@@ -16,10 +16,11 @@ typedef struct node {
 
 void displayStudList(Studlist List);
 void insertFirstUnique(Studrec student, Studlist *List);
+Studrec deleteStudRecordByID(int ID, Studlist *List);
 
 int main(){
     Studlist List = NULL;
-    Studrec student1, student2;
+    Studrec student1, student2, deleted;
     student1.ID = 101;
     strcpy(student1.LN, "Tumapon");
     strcpy(student1.FN, "Nikolai Franz");
@@ -27,7 +28,7 @@ int main(){
     strcpy(student1.course, "BSIT");
     student1.yearLevel = 1;
 
-    student2.ID = 101;
+    student2.ID = 102;
     strcpy(student2.LN, "Tumapon");
     strcpy(student2.FN, "Nikolai Franz");
     student2.MI = 'R';
@@ -36,6 +37,14 @@ int main(){
 
     insertFirstUnique(student1, &List);
     insertFirstUnique(student2, &List);
+    deleted = deleteStudRecordByID(103, &List);
+    printf("DELETED");
+    printf("\nID: %d\n", deleted.ID);
+    printf("First Name: %s\n", deleted.FN);
+    printf("Last Name: %s\n", deleted.LN);
+    printf("Middle Initial: %c\n", deleted.MI);
+    printf("Course: %s\n", deleted.course);
+    printf("Year Level: %d\n", deleted.yearLevel);
     displayStudList(List);
     return 0;
 }
@@ -64,4 +73,25 @@ void insertFirstUnique(Studrec newStud, Studlist *A){
             *A = temp; //set new node as the first node
         }
     }
+}
+
+Studrec deleteStudRecordByID(int ID, Studlist *A){
+    Studlist *trav = A;
+    Studrec ret;
+    struct node *temp;
+    for(trav = A; *trav != NULL && (*trav)->stud.ID != ID; trav = &(*trav)->link){}
+    if(trav == NULL){
+        ret = (*trav)->stud;
+        temp = *trav;
+        *trav = (*trav)->link;
+        free(temp);
+    } else {
+        ret.ID = 0;
+        strcpy(ret.FN, "XXXXX");
+        strcpy(ret.LN, "XXXXX");
+        ret.MI = 'X';
+        strcpy(ret.course, "XXXXX");
+        ret.yearLevel = 0;
+    }
+    return ret;
 }
