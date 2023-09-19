bce0610b357a81c4d6d0548fb813512e5d93221f
Nikolai Franz <franztumapon13@gmail.com>
2023-09-19 11:15:20 +0800

    bit-wise

---
+++ Bit-Wise/BitWise.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void displayBitPattern(int num);
+
+int main() {
+    int num = 0;
+
+    printf("Bit pattern: ");
+    displayBitPattern(num);
+
+    return 0;
+}
+
+void displayBitPattern(int num) {
+    int numBits = sizeof(num) * 8; // Determine the number of bits in an integer
+
+    // Iterate through each bit from left to right
+    for (int i = numBits - 1; i >= 0; i--) {
+        // Use a bitmask to extract the current bit
+        int bit = (num >> i) & 1;
+
+        // Print the bit
+        printf("%d", bit);
+
+        // Add a space after every 4 bits for readability
+        if (i % 4 == 0)
+            printf(" ");
+    }
+
+    printf("\n");
+}
